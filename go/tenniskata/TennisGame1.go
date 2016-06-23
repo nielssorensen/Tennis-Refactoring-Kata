39df9d5c153224c778ab1d8a7a7d2fa094ac7a62
Niels Sorensen <niels.ws@gmail.com>
2016-06-23 19:53:30 -0400

    Remove string literal comparison

--- go/tenniskata/TennisGame1.go
+++ go/tenniskata/TennisGame1.go
@@ -16,8 +16,8 @@ func TennisGame1(player1Name string, player2Name string) TennisGame {
 }
 
 func (game *tennisGame1) WonPoint(playerName string) {
-	if playerName == "player1" {
-		game.m_score1 += 1
+	if playerName == game.player1Name {
+		game.m_score1 += 1 
 	} else {
 		game.m_score2 += 1
 	}
